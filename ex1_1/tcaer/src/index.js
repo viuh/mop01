9526bf50d0d4e836c1b925c059d25c9b48fc4ef3
viuh <ztuula+mooc18@gmail.com>
2018-04-01 21:03:06 +0300

    ex 1.5

--- ex1_1/tcaer/src/index.js
+++ ex1_1/tcaer/src/index.js
@@ -51,21 +51,23 @@ const Osa = (props) => {
 
 
 const App = () => {
-    const kurssi = 'Half Stack -sovelluskehitys'
-    const osat = [
-        {
-          nimi: 'Reactin perusteet',
-          tehtavia: 10
-        },
-        {
-          nimi: 'Tiedonvälitys propseilla',
-          tehtavia: 7
-        },
-        {
-          nimi: 'Komponenttien tila',
-          tehtavia: 14
-        }
-      ]
+    const kurssi = {
+        nimi: 'Half Stack -sovelluskehitys',
+        osat: [
+          {
+            nimi: 'Reactin perusteet',
+            tehtavia: 10
+          },
+          {
+            nimi: 'Tiedonvälitys propseilla',
+            tehtavia: 7
+          },
+          {
+            nimi: 'Komponenttien tila',
+            tehtavia: 14
+          }
+        ]
+    }
 
     return (
     <div>
@@ -74,9 +76,9 @@ const App = () => {
       <p>{osa2} {tehtavia2}</p>
       <p>{osa3} {tehtavia3}</p>
       <p>yhteensä {tehtavia1 + tehtavia2 + tehtavia3} tehtävää</p>*/}
-      <Otsikko kurssi={kurssi}/>
-      <Sisalto osat={osat}/>
-      <Yhteensa osat={osat} />
+      <Otsikko kurssi={kurssi.nimi}/>
+      <Sisalto osat={kurssi.osat}/>
+      <Yhteensa osat={kurssi.osat} />
     </div>
   )
 }
